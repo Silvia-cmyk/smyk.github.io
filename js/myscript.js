1df0e8806ad1f93e83a1a2050bd300c564437181
Via <seaforest01225@gmail.com>
2025-04-24 15:00:37 +0800

    update 😭

--- js/myscript.js
+++ js/myscript.js
@@ -1,57 +1,57 @@
 const data = [
     {
-        img: "asset/portfolio/picheart.jpg",
+        img: "assets/portfolio/picheart.jpg",
         title: "Step 1: Browse by Category",
         desc: "Choose from categories like Web Design, Interactive Media, and Branding.",
         link:"https://reurl.cc/YYE1yO"
     },
     {
-        img: "asset/portfolio/watermask.png",
+        img: "assets/portfolio/watermask.png",
         title: "Step 2: Choose a Template",
         desc: "Each project includes insights into my creative process and techniques."
     },
     {
-        img: "asset/portfolio/smallcollection.jpg",
+        img: "assets/portfolio/smallcollection.jpg",
         title: "Step 3: Customize Your Design",
         desc: "Easily customize colors, fonts, and layouts to fit your brand."
     },
     {
-        img: "asset/portfolio/minihouseblock.jpg",
+        img: "assets/portfolio/minihouseblock.jpg",
         title: "Step 4: Preview and Download",
         desc: "Preview your design and download it in various formats."
     },
     {
-        img: "asset/portfolio/particlesimulation.jpg",
+        img: "assets/portfolio/particlesimulation.jpg",
         title: "Step 5: Share Your Work",
         desc: "Showcase your design on social media or your portfolio."
     },
     {
-        img: "asset/portfolio/openctisample.png",
+        img: "assets/portfolio/openctisample.png",
         title: "Step 6: Get Inspired",
         desc: "Explore other users' designs for inspiration and ideas."
     },
     {
-        img: "asset/portfolio/nightmarket.png",
+        img: "assets/portfolio/nightmarket.png",
         title: "Step 7: Join the Community",
         desc: "Connect with fellow designers and share your work."
     },
     {
-        img: "asset/portfolio/attacksample.png",
+        img: "assets/portfolio/attacksample.png",
         title: "Step 8: Join the Community",
         desc: "Connect with fellow designers and share your work 1."
     },
     {
-        img: "asset/portfolio/grafanadata.jpg",
+        img: "assets/portfolio/grafanadata.jpg",
         title: "Step 9: Join the Community",
         desc: "Connect with fellow designers and share your work 2."
     },
     {
-        img: "asset/portfolio/baiheebook.jpg",
+        img: "assets/portfolio/baiheebook.jpg",
         title: "Step 10: Join the Community",
         desc: "Connect with fellow designers and share your work 3."
     },
     {
-        img: "asset/portfolio/sharkpreventiondiary.png",
+        img: "assets/portfolio/sharkpreventiondiary.png",
         title: "Step 11: Join the Community",
         desc: "Connect with fellow designers and share your work 4."
     }
